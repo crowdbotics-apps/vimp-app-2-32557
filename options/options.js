5a896cb4de0a8970174821580def5fdeec30f4f7
Crowdbotics <team@crowdbotics.com>
2021-12-23 10:13:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vimp_app_2_32557",
+  url: "https://vimp_app_2_32557.botics.co",
+  api: "https://vimp_app_2_32557.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
